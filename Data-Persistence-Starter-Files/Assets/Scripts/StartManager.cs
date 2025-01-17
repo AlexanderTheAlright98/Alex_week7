65f998506e30e803b6fca6a307dcf33d903294d4
AlexanderTheAlright98 <Alexanderthealright@outlook.com>
2025-01-17 15:48:43 +0000

    Week 7 Final Commit
    Added:
    - Challenge project files

---
+++ Data-Persistence-Starter-Files/Assets/Scripts/StartManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.IO;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class StartManager : MonoBehaviour
+{
+    public static StartManager instance { get; private set; }
+    public TMP_InputField nameInput;
+    public string userName;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        userName = nameInput.text;
+    }
+    public void startGame()
+    {
+        PlayerPrefs.SetString("PlayerName", userName);
+        SceneManager.LoadScene(1);
+    }
+}
